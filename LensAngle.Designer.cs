486dd3b7fb165884a0af55c9297113a8b56c1b76
yama-ten <tenshi@tokachi-lab.com>
2021-10-17 22:39:16 +0900

    first

---
+++ LensAngle.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace LensAngle
+{
+	partial class LensAngle
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+			this.DiagonalAngle = new System.Windows.Forms.TextBox();
+			this.lbl_Diagonal = new System.Windows.Forms.Label();
+			this.lbl_35mm = new System.Windows.Forms.Label();
+			this.Lens_35mm = new System.Windows.Forms.TextBox();
+			this.lbl_APS_C = new System.Windows.Forms.Label();
+			this.Lens_Apsc = new System.Windows.Forms.TextBox();
+			this.lbl_H = new System.Windows.Forms.Label();
+			this.AngleH = new System.Windows.Forms.TextBox();
+			this.lbl_V = new System.Windows.Forms.Label();
+			this.AngleV = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.White;
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(486, 325);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Resize += new System.EventHandler(this.pictureBox1_Resize);
+			// 
+			// hScrollBar1
+			// 
+			this.hScrollBar1.Location = new System.Drawing.Point(12, 340);
+			this.hScrollBar1.Name = "hScrollBar1";
+			this.hScrollBar1.Size = new System.Drawing.Size(486, 19);
+			this.hScrollBar1.TabIndex = 7;
+			this.hScrollBar1.TabStop = true;
+			this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+			// 
+			// DiagonalAngle
+			// 
+			this.DiagonalAngle.BackColor = System.Drawing.Color.DodgerBlue;
+			this.DiagonalAngle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.DiagonalAngle.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.DiagonalAngle.ForeColor = System.Drawing.Color.White;
+			this.DiagonalAngle.Location = new System.Drawing.Point(88, 375);
+			this.DiagonalAngle.Multiline = true;
+			this.DiagonalAngle.Name = "DiagonalAngle";
+			this.DiagonalAngle.Size = new System.Drawing.Size(52, 27);
+			this.DiagonalAngle.TabIndex = 8;
+			this.DiagonalAngle.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			// 
+			// lbl_Diagonal
+			// 
+			this.lbl_Diagonal.AutoSize = true;
+			this.lbl_Diagonal.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lbl_Diagonal.Location = new System.Drawing.Point(22, 379);
+			this.lbl_Diagonal.Name = "lbl_Diagonal";
+			this.lbl_Diagonal.Size = new System.Drawing.Size(60, 17);
+			this.lbl_Diagonal.TabIndex = 9;
+			this.lbl_Diagonal.Text = "Diagonal";
+			// 
+			// lbl_35mm
+			// 
+			this.lbl_35mm.AutoSize = true;
+			this.lbl_35mm.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lbl_35mm.Location = new System.Drawing.Point(301, 380);
+			this.lbl_35mm.Name = "lbl_35mm";
+			this.lbl_35mm.Size = new System.Drawing.Size(44, 17);
+			this.lbl_35mm.TabIndex = 11;
+			this.lbl_35mm.Text = "35mm";
+			// 
+			// Lens_35mm
+			// 
+			this.Lens_35mm.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.Lens_35mm.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.Lens_35mm.Location = new System.Drawing.Point(346, 376);
+			this.Lens_35mm.Name = "Lens_35mm";
+			this.Lens_35mm.Size = new System.Drawing.Size(42, 25);
+			this.Lens_35mm.TabIndex = 10;
+			this.Lens_35mm.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			// 
+			// lbl_APS_C
+			// 
+			this.lbl_APS_C.AutoSize = true;
+			this.lbl_APS_C.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lbl_APS_C.Location = new System.Drawing.Point(397, 380);
+			this.lbl_APS_C.Name = "lbl_APS_C";
+			this.lbl_APS_C.Size = new System.Drawing.Size(43, 17);
+			this.lbl_APS_C.TabIndex = 13;
+			this.lbl_APS_C.Text = "APS-C";
+			// 
+			// Lens_Apsc
+			// 
+			this.Lens_Apsc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.Lens_Apsc.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.Lens_Apsc.Location = new System.Drawing.Point(440, 375);
+			this.Lens_Apsc.Name = "Lens_Apsc";
+			this.Lens_Apsc.Size = new System.Drawing.Size(42, 25);
+			this.Lens_Apsc.TabIndex = 12;
+			this.Lens_Apsc.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			// 
+			// lbl_H
+			// 
+			this.lbl_H.AutoSize = true;
+			this.lbl_H.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lbl_H.Location = new System.Drawing.Point(157, 379);
+			this.lbl_H.Name = "lbl_H";
+			this.lbl_H.Size = new System.Drawing.Size(17, 17);
+			this.lbl_H.TabIndex = 15;
+			this.lbl_H.Text = "H";
+			// 
+			// AngleH
+			// 
+			this.AngleH.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+			this.AngleH.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.AngleH.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.AngleH.Location = new System.Drawing.Point(175, 376);
+			this.AngleH.Name = "AngleH";
+			this.AngleH.Size = new System.Drawing.Size(42, 25);
+			this.AngleH.TabIndex = 14;
+			this.AngleH.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			// 
+			// lbl_V
+			// 
+			this.lbl_V.AutoSize = true;
+			this.lbl_V.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lbl_V.Location = new System.Drawing.Point(224, 378);
+			this.lbl_V.Name = "lbl_V";
+			this.lbl_V.Size = new System.Drawing.Size(16, 17);
+			this.lbl_V.TabIndex = 17;
+			this.lbl_V.Text = "V";
+			// 
+			// AngleV
+			// 
+			this.AngleV.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+			this.AngleV.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.AngleV.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.AngleV.Location = new System.Drawing.Point(241, 376);
+			this.AngleV.Name = "AngleV";
+			this.AngleV.Size = new System.Drawing.Size(42, 25);
+			this.AngleV.TabIndex = 16;
+			this.AngleV.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			// 
+			// LensAngle
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(510, 414);
+			this.Controls.Add(this.lbl_V);
+			this.Controls.Add(this.AngleV);
+			this.Controls.Add(this.lbl_H);
+			this.Controls.Add(this.AngleH);
+			this.Controls.Add(this.lbl_APS_C);
+			this.Controls.Add(this.Lens_Apsc);
+			this.Controls.Add(this.lbl_35mm);
+			this.Controls.Add(this.Lens_35mm);
+			this.Controls.Add(this.lbl_Diagonal);
+			this.Controls.Add(this.DiagonalAngle);
+			this.Controls.Add(this.hScrollBar1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "LensAngle";
+			this.Text = "LensAngleForm";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.HScrollBar hScrollBar1;
+		private System.Windows.Forms.TextBox DiagonalAngle;
+		private System.Windows.Forms.Label lbl_Diagonal;
+		private System.Windows.Forms.Label lbl_35mm;
+		private System.Windows.Forms.TextBox Lens_35mm;
+		private System.Windows.Forms.Label lbl_APS_C;
+		private System.Windows.Forms.TextBox Lens_Apsc;
+		private System.Windows.Forms.Label lbl_H;
+		private System.Windows.Forms.TextBox AngleH;
+		private System.Windows.Forms.Label lbl_V;
+		private System.Windows.Forms.TextBox AngleV;
+	}
+}
+
---
+++ LensAngle.cs
@@ -0,0 +1,193 @@
+﻿/****************************************************************
+ * title: レンズの画角と焦点距離
+ * file: Form1.cs
+ * version: 1.1
+ * date: 2021.10.04
+ * ver 1.0 / 2021.10.04
+ * ver 1.1 / 2021.10.17
+ * copyright: yama-ten
+ */
+
+//2021.10.17
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LensAngle
+{
+	public partial class LensAngle : Form
+	{		public LensAngle()
+		{
+			InitializeComponent();
+			this.Text = "画角と焦点距離";
+		}
+
+		Rectangle box;
+		Point center;
+		Bitmap canvas;
+		Rectangle bigRect = new Rectangle(0,0, 400, 400);
+		Rectangle littleRect = new Rectangle(0,0, 100, 100);
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			center.X = pictureBox1.Width/2;
+			center.Y = pictureBox1.Height*3/4;
+
+			//描画先とするImageオブジェクトを作成する
+			canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+			//draw_LensAngle(23);
+			hScrollBar1.Minimum = 0;
+			hScrollBar1.Maximum = 909;
+			hScrollBar1.SmallChange = 1;
+			hScrollBar1.Value = 230; // 23.0 degree (46/2 deg.)
+			draw_LensAngle((double)hScrollBar1.Value/10);
+		}
+
+
+		int cvtAngle(int angle) {
+			return (360 - angle - 90) %360; 
+		}
+
+
+		void draw_BigBox(Graphics g)
+		{
+			Pen p = new Pen(Color.Black, 1);
+
+			int width = bigRect.Width;
+			int height = bigRect.Height/2;
+			int left = center.X - bigRect.Width/2;
+			int	top = center.Y - height;
+			
+			g.DrawRectangle(p, left, top, width, height);
+		}
+
+		void draw_LittleBox(Graphics g)
+		{
+			Pen p = new Pen(Color.Black, 1);
+
+			int	width = littleRect.Width;
+			int	height = littleRect.Height/2;
+			int	left = center.X - width/2;
+			int	top = center.Y;
+			
+			g.DrawRectangle(p, left, top, width, height);
+		}
+
+		void draw_CenterLine(Graphics g)
+		{
+			int width = bigRect.Width;
+			int height = bigRect.Height/2;
+			int left = center.X - bigRect.Width/2;
+			int top = center.Y - bigRect.Height/2;
+			int bottom = center.Y + littleRect.Height/2;
+			int length = bottom - top;
+			g.DrawLine(Pens.Red, center.X, top, center.X, bottom);
+		}
+
+		void draw_pie(double degree, Graphics g, Rectangle rect, Pen p)
+		{
+			draw_BigPie(degree, g, bigRect, p);
+			draw_LittlePie(degree, g, littleRect, p);
+			draw_LittleBox(g);
+			draw_BigBox(g);
+			draw_CenterLine(g);
+		}
+
+
+		void draw_BigPie(double degree, Graphics g, Rectangle rect, Pen p)
+		{
+			float angle = cvtAngle(0);
+			float width = bigRect.Width;
+			float height = bigRect.Height;
+			float left = center.X - width/2;
+			float top = center.Y - height/2;
+			g.DrawPie(p, left, top, width, height, angle,(float)-degree);
+			g.DrawPie(p, left, top, width, height, angle,(float)degree);
+		}
+
+		void draw_LittlePie(double degree, Graphics g, Rectangle rect, Pen p)
+		{
+			float angle = cvtAngle(180);
+			float width = littleRect.Width;
+			float height = littleRect.Height;
+			float left = center.X - width/2;
+			float top = center.Y - height/2;
+			g.DrawPie(p, left, top, width, height, angle, (float)-degree);
+			g.DrawPie(p, left, top, width, height, angle, (float)degree);
+		}
+
+		private void pictureBox1_Resize(object sender, EventArgs e)
+		{
+			Control ctrl = (Control)sender;
+			box = new Rectangle(0, 0, ctrl.Size.Width, ctrl.Size.Height);
+			center = new Point(box.Width/2, box.Height/2);
+		}
+
+
+		//double last_degree = -1;
+		private void draw_LensAngle(double degree)
+		{
+			canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+			Graphics g = Graphics.FromImage(canvas);
+			Pen p = new Pen(Color.Blue, 1);
+
+			// 画角
+			DiagonalAngle.Text = (2*degree).ToString("##0.0 ");
+
+			// 35mm の焦点距離
+			double tan = Math.Tan(degree*Math.PI/180);
+			double diagonal = Math.Sqrt(36*36+24*24);
+			double f = (diagonal/2)/tan;
+			Lens_35mm.Text = (f+0.05).ToString("###0.0 ");
+			
+			// 水平画角
+			double ah = Math.Atan((36/2)/f)/Math.PI*180;
+			AngleH.Text = (2*ah).ToString("##0.0 ");
+
+			// 垂直画角
+			double av = Math.Atan((24/2)/f)/Math.PI*180;
+			AngleV.Text = (2*av).ToString("##0.0 ");
+
+			// APS-C(24mm) の焦点距離
+			diagonal = Math.Sqrt(24*24+16*16);
+			f = (diagonal/2)/tan;
+			Lens_Apsc.Text = (f+0.05).ToString("###0.0 ");
+
+			int top = center.Y - bigRect.Height/2;
+			int left = center.X - bigRect.Width/2;
+			int right = center.X + bigRect.Width/2;
+			int bottom = center.Y + bigRect.Height/2;
+			//int length = 0;
+			int width = bigRect.Width;
+			int height = bigRect.Height;
+
+			draw_pie(av, g, bigRect, new Pen(Color.Pink,1)); 
+			draw_pie(ah, g, bigRect, new Pen(Color.LightGreen,1)); 
+			draw_pie(degree, g, bigRect, new Pen(Color.Blue,1)); 
+
+			//PictureBox1に表示する
+			pictureBox1.Image = canvas;
+
+			//リソースを解放する
+			p.Dispose();
+			g.Dispose();
+		}
+
+
+		private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+		{
+			ScrollBar ctrl = (ScrollBar)sender;
+			double degree =  (double)(ctrl.Value)/10;
+			draw_LensAngle(degree);
+		}
+	}
+
+}
